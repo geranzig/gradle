bc246b5de5f483483d5a778d000ccf1d184a29b2
Geranzig <gerardo.cornejo.gonzalez@gmail.com>
2022-11-21 21:26:00 -0300

    build maven or gradle

--- gradle.groovy
+++ gradle.groovy
@@ -11,7 +11,7 @@ def Test(){
 }
 
 def Sonar(){
-    sh "./gradlew sonar:sonar -Dsonar.projectKey=gradle-test -Dsonar.host.url=http://178.128.155.87:9000  -Dsonar.login=sqp_10c86add5add0c41abe71d9f0377f11070a08e57 -Dsonar.java.binaries=build"
+    sh "./gradlew sonarqube -Dsonar.projectKey=gradle-test -Dsonar.host.url=http://178.128.155.87:9000  -Dsonar.login=sqp_10c86add5add0c41abe71d9f0377f11070a08e57 -Dsonar.java.binaries=build"
 
 }
 
